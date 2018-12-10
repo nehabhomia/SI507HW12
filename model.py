301b77b525992ff77ea1d05e78d156b3f66653c2
Neha Bhomia <nehabhomia@hotmail.co.uk>
2018-12-09 22:11:23 -0500

    part 2

--- model.py
+++ model.py
@@ -12,12 +12,17 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
+counter = -1
+admin_entries = []
 
 def init():
-    global entries
+    global entries, next_id, counter
     try:
         f = open(GUESTBOOK_ENTRIES_FILE)
         entries = json.loads(f.read())
+        counter = int(entries[-1]['MAX_ID'])
+        next_id = counter + 1
         f.close()
     except:
         entries = []
@@ -27,13 +32,21 @@ def get_entries():
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id, counter
     now = datetime.now()
+    next_id = counter + 1
     time_string = now.strftime("%b %d, %Y %-I:%M %p")
     # if you have an error using this format, just use
     # time_string = str(now)
     entry = {"author": name, "text": text, "timestamp": time_string}
+    counter = counter + 1
     entries.insert(0, entry) ## add to front of list
+    
+    try:
+        entries[-1]['MAX_ID'] = str(next_id)
+    except:
+        entries.insert({'MAX_ID': str(next_id)})
+    
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
         dump_string = json.dumps(entries)
