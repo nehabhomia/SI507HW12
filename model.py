473133ada8277942f2db91942227c0b9a7735d1d
Neha Bhomia <nehabhomia@hotmail.co.uk>
2018-12-09 21:51:25 -0500

    skeleton from lecture

---
+++ model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  9 21:49:31 2018
+
+@author: nehabhomia
+"""
+
+
+import json
+from datetime import datetime
+
+GUESTBOOK_ENTRIES_FILE = "entries.json"
+entries = []
+
+def init():
+    global entries
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE)
+        entries = json.loads(f.read())
+        f.close()
+    except:
+        entries = []
+
+def get_entries():
+    global entries
+    return entries
+
+def add_entry(name, text):
+    global entries, GUESTBOOK_ENTRIES_FILE
+    now = datetime.now()
+    time_string = now.strftime("%b %d, %Y %-I:%M %p")
+    # if you have an error using this format, just use
+    # time_string = str(now)
+    entry = {"author": name, "text": text, "timestamp": time_string}
+    entries.insert(0, entry) ## add to front of list
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        dump_string = json.dumps(entries)
+        f.write(dump_string)
+        f.close()
+    except:
+        print("ERROR! Could not write entries to file.")
